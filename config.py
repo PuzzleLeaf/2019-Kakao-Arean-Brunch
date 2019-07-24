21f53506330b63dfc1f30d9318c37da62da392f6
puzzleleaf <cmtyx2@naver.com>
2019-07-24 12:02:30 +0900

    test 로 변경

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # dev 유저
-predict_res = './res/predict/dev.users'
+# predict_res = './res/predict/dev.users'
 
 # test 유저
-# predict_res = './res/predict/test.users'
+predict_res = './res/predict/test.users'
